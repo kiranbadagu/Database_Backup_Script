ddc8d14a227415dbee5f1c26c67f25540bfdabce
Kiran Badagu <59598096+kiranbadagu@users.noreply.github.com>
2020-04-20 23:00:19 +0530

    initial commit

---
+++ backup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Database credentials
+user="MYSQL_ROOT_NAME"
+password="MYSQL_ROOT_PASSWORD"
+host="localhost"
+server=$localhost:3306
+db_name="DATABASE_NAME"
+
+# other options
+backup_path="PATH_TO_BACKUP"
+bucket_name="GCP_BUCKET_NAME"
+date=$(date +"%H%M%S%d%b%Y")
+
+# Set default file permissions
+chmod 444
+
+# Dump database into SQL file
+mysqldump --user=$user --password=$password --host=$host $db_name > $backup_path$server$db_name$date.sql
+
+# Delete files older than 7 days from local machine
+find $backup_path/* -mtime +7 -exec rm {} \;
+
+# Synchronize local backup directory to a Google Cloud Storage bucket
+gsutil cp  $backup_path$server$db_name$date.sql   gs://$bucket_name
